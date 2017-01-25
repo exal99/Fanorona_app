453f690131648f879fdf56c57a7586432831f55f
Alexander Simko <asimko94@gmail.com>
2017-01-25 18:24:00 +0100

    Added confermation window, back button support and removed title bar

--- app/src/main/java/sweden/alexander/fanorona/MainActivity.java
+++ app/src/main/java/sweden/alexander/fanorona/MainActivity.java
@@ -1,20 +1,49 @@
 package sweden.alexander.fanorona;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.AlertDialog;
+import android.view.Window;
+import android.view.WindowManager;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private Fanorona game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         FragmentManager fragmentManager = getFragmentManager();
-        Fragment fragment = new Fanorona();
+        game = new Fanorona();
+        Fragment fragment = game;
         fragmentManager.beginTransaction()
                 .replace(R.id.container, fragment)
                 .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (game.getCurrScreen() == Fanorona.Screen.GAME) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Quit current game?")
+                    .setMessage("Do you really want to exit the current game? All progress will be lost")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            game.back();
+                        }})
+                    .setNegativeButton(android.R.string.no, null).show();
+        } else {
+            game.back();
+        }
     }
 }
