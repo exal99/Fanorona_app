1dbc0c0518bc1806495b511886341a0519feb49e
Alexander Simko <asimko94@gmail.com>
2017-01-29 21:09:41 +0100

    Added a timer dialog xml

--- fanorona/src/main/java/sweden/alexander/fanorona/MainActivity.java
+++ fanorona/src/main/java/sweden/alexander/fanorona/MainActivity.java
@@ -1,5 +1,6 @@
 package sweden.alexander.fanorona;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,7 +9,8 @@ import android.app.FragmentManager;
 import android.app.AlertDialog;
 import android.view.Window;
 import android.view.WindowManager;
-
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,13 +23,23 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
-        FragmentManager fragmentManager = getFragmentManager();
-        game = new Fanorona();
-        Fragment fragment = game;
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, fragment)
-                .commit();
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Conferm?")
+                .setView(R.layout.timer_dialog)
+                .create();
+
+//        NumberPicker np = (NumberPicker) dialog.findViewById(R.id.minutes);
+//        np.setMaxValue(60);
+//        np.setMinValue(0);
+        dialog.show();
+        //setContentView(R.layout.timer_dialog);
+//        setContentView(R.layout.activity_main);
+//        FragmentManager fragmentManager = getFragmentManager();
+//        game = new Fanorona();
+//        Fragment fragment = game;
+//        fragmentManager.beginTransaction()
+//                .replace(R.id.container, fragment)
+//                .commit();
     }
 
     @Override
